0dfb2793d46351f9ea6bff6561224f0ed20aec58
aditi <aditi08092003@gmail.com>
2026-03-07 13:54:44 +0530

    Missing Number Day-2

---
+++ com/practice/MissingNumberInAArray.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class MissingNumberInAArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr =  new int[n];
+        int sum = 0;
+        System.out.println("Enter elements :");
+        for(int  i = 0 ; i < n ; i ++ ){
+            arr[i] = sc.nextInt();
+            sum+=arr[i];
+        }
+        System.out.println(sum/n);
+        sc.close();
+    }
+}
